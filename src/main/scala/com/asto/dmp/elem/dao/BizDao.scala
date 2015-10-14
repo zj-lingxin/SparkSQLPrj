93e0b446f1a471242042997e6c8162afdc95c7d3
zj-lingxin <690094095@qq.com>
2015-10-15 00:30:04 +0800

    2015-10-15 0:29 增加

--- src/main/scala/com/asto/dmp/elem/dao/BizDao.scala
+++ src/main/scala/com/asto/dmp/elem/dao/BizDao.scala
@@ -2,19 +2,55 @@ package com.asto.dmp.elem.dao
 
 import com.asto.dmp.elem.base.DataSource
 
+import scala.collection.SortedMap
+
 /**
  * Created by lingx on 2015/10/14.
  */
 class BizDao extends DataSource {
-  def orderProperty(properties: String = "*") = {
-    if (properties == "*") {
+  val queryString = "id,user_id"
+//  def orderProperty(query: String) = {
+//    if (properties == "*") {
+//
+//    } else {
+//
+//    }
+//    sqlContext.sql(s"select $properties from order ").map(a => (a(0).toString, a(1)))
+//  }
 
-    } else {
 
-    }
-    sqlContext.sql(s"select $properties from order ").map(a => (a(0).toString, a(1)))
+}
+object BizDao extends DataSource{
+  def toTuple[A <: Object ](as:Seq[A]) = {
+    val tupleClass = Class.forName("scala.Tuple" + as.size)
+    tupleClass.getConstructors.apply(0).newInstance(as:_*).asInstanceOf[Product]
   }
-  private def foo() = {
 
+  private def foo(selectedProps: String) = {
+    val (indexMap,typeMap) = getIndexAndTypeMaps(CREDIT_TRADE_VIEW)
+    val propNames = selectedProps.split(",")
+
+    //sqlContext.sql(s"select $selectedProps from order ").map(a => toTuple(a.toSeq))
+    List[List[String]](List("1","ss"),List("2","ss1")).map(a => toTuple(a.toSeq)).foreach(println)
+  }
+
+  private def getIndexAndTypeMaps(schema: String) = {
+    val indexMap = scala.collection.mutable.Map[String,Int]()
+    val typeMap  = scala.collection.mutable.Map[String,String]()
+    val elems = schema.split(",")
+    var index = 0
+    for(e <- elems){
+      val sp = e.split(":")
+      val propertyName = sp(0).trim
+      val propertyType = sp(1).trim
+      indexMap += (propertyName -> index)
+      index += 1
+      typeMap += (propertyName -> propertyType)
+    }
+    (indexMap,typeMap)
+  }
+  val CREDIT_TRADE_VIEW    = "id:String,user_id:Long,shop_id:String,gmt_target:Date,pay_money:Double"
+  def main(args: Array[String]) {
+    foo("id,shop_id")
   }
 }
